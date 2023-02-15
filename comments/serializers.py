2482b96c58b674a8915c902080d6ce11342824f9
JoeQuigley1 <joeyjtq@hotmail.com>
2023-02-15 16:34:12 +0000

    Fix database error for comments model

--- comments/serializers.py
+++ comments/serializers.py
@@ -2,7 +2,11 @@ from rest_framework import serializers
 from .models import Comment
 
 
-class CommentSerlizer(serializers.ModelSerializer):
+class CommentSerializer(serializers.ModelSerializer):
+    """
+    Serializer for the Comment model
+    Adds three extra fields when returning a list of Comment instances
+    """
     owner = serializers.ReadOnlyField(source='owner.username')
     is_owner = serializers.SerializerMethodField()
     profile_id = serializers.ReadOnlyField(source='owner.profile.id')
@@ -15,11 +19,14 @@ class CommentSerlizer(serializers.ModelSerializer):
     class Meta:
         model = Comment
         fields = [
-            'id', 'owner', 'is_owner', 'profile_id',
-            'profile_image', 'post', 'created_at', 'updated_at',
-            'content'
+            'id', 'owner', 'is_owner', 'profile_id', 'profile_image',
+            'post', 'created_at', 'updated_at', 'content'
         ]
 
 
-class CommentDetailSerlizer():
+class CommentDetailSerializer(CommentSerializer):
+    """
+    Serializer for the Comment model used in Detail view
+    Post is a read only field so that we dont have to set it on each update
+    """
     post = serializers.ReadOnlyField(source='post.id')
